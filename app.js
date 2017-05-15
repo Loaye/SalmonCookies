014afeb2c8fb1a97bdfe50824ad8b1ee4d80d57b
Loaye <jr.carstens00@gmail.com>
2017-05-15 13:36:15 -0700

    Created 5 objects per location

---
+++ app.js
@@ -0,0 +1,40 @@
+//Creating object with min, max, avg
+var firstAndPike = {
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
+
+firstAndPike();
+
+var seaTac = {
+  min: 3,
+  max: 24,
+  avg: 1.2,
+};
+
+seaTac();
+
+var seattleCenter = {
+  min: 11,
+  max: 38,
+  avg: 3.7
+};
+
+seattleCenter();
+
+var capHill = {
+  min: 20,
+  max: 38,
+  avg: 2.3,
+}
+
+capHill();
+
+var alki = {
+  min: 2,
+  max: 16,
+  avg: 4.6,
+};
+
+alki();
