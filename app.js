b10aa6b2f55133be7c3e2863c78beb872be6a2aa
Loaye <jr.carstens00@gmail.com>
2017-05-15 16:07:00 -0700

    changed variables to array to push keywords

--- app.js
+++ app.js
@@ -1,63 +1,48 @@
 //Creating object with min, max, avg
-var firstAndPike = {
-  min: 23,
-  max: 65,
-  avg: 6.3,
-};
+var storeLocation = [
+  {
+    nameP: 'Pike',
+    min: 23,
+    max: 65,
+    avg: 6.3,
+  },
+  {
+    nameS: 'SeaTac',
+    min: 3,
+    max: 24,
+    avg: 1.2,
+  },
+  {
+    nameSC: 'Seattle Center',
+    min: 11,
+    max: 38,
+    avg: 3.7
+  },
+  {
+    nameCH: 'Captiol Hill',
+    min: 20,
+    max: 38,
+    avg: 2.3,
+  },
+  {
+    nameA: 'Alki',
+    min: 2,
+    max: 16,
+    avg: 4.6,
+  },
+];
 
-firstAndPike();
+//Function that generates random cookie ammount per customer
 
-var seaTac = {
-  min: 3,
-  max: 24,
-  avg: 1.2,
-};
-
-seaTac();
-
-var seattleCenter = {
-  min: 11,
-  max: 38,
-  avg: 3.7
-};
-
-seattleCenter();
-
-var capHill = {
-  min: 20,
-  max: 38,
-  avg: 2.3,
-};
-
-capHill();
-
-var alki = {
-  min: 2,
-  max: 16,
-  avg: 4.6,
-};
-
-alki();
-
-//Creating function to generate random customers per hour
-var oneBetweenHundred = (Math.floor(Math.random() * 100));
-
-function randomCustomerAmmount(){
-  return oneBetweenHundred();
+function hourlyCookie(min, max, avg){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  var people = Math.floor(Math.random() * (max - min)) + min;
+  console.log('Total People;' + avg);
+  return(people * avg);
 }
 
-randomCustomerAmmount();
-
-//Function that generates random cookie ammount between min and max
-
-function cookieAnHour(min, max, avg){
-  cookieMin = Math.ceil(min);
-  cookieMax = Math.floor(max);
-  var cookieAvg = Math.floor(Math.random() * (cookieMax - cookieMin)) + cookieMin;
-  return(cookieAvg);
-}
-
-cookieAnHour();
+hourlyCookie();
 
 //function that creates list of store and grabs cookie per hour, then creates a list over the business day
 function listStore(store){
