3fdd645a12ed977bc9c947a243fcfad04ee660c6
Loaye <jr.carstens00@gmail.com>
2017-05-17 23:13:59 -0700

    end day push

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
  }
 
 //Creating function to fill table header with time
-function hours(){
+function hours()
   var time = ['6AM:', '7AM:', '8AM:', '9AM:', '10AM:', '11AM:', '12PM:', '1PM:', '2PM:', '3PM:', '4PM:', '5PM:', '6PM:', '7PM:', 'Total:'];
   var table = document.getElementsByClassName('operation_hours');
   var operationArr = [];
