d037542bcb111834db4e1d3c9a0bf4aa57be32cd
Loaye <jr.carstens00@gmail.com>
2017-05-18 13:30:03 -0700

    modified files

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-'use strict'
+'use strict';
  //creating a Constructor for a store location
- function Store(loc, min, max, avg){
-   this.loc = location;
-   this.min = min;
-   this.max = max;
-   this.avg = avg;
- }
+function Store(loc, min, max, avg){
+  this.loc = location;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+}
 
  //Function that generates total cookie ammount for random customer per hour
- Store.prototype.hourly = function(){
-   this.min = Math.ceil(this.min);
-   this.max = Math.floor(this.max);
-   var people = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-   console.log('Total People:' + this.avg);
-   return(people * this.avg);
- }
+Store.prototype.hourly = function(){
+  this.min = Math.ceil(this.min);
+  this.max = Math.floor(this.max);
+  var people = Math.floor(Math.random() * (this.max - this.min)) + this.min;
+  console.log('Total People:' + this.avg);
+  return(people * this.avg);
+};
 
 //Creating function to fill table header with time
-function hours()
-  var time = ['6AM:', '7AM:', '8AM:', '9AM:', '10AM:', '11AM:', '12PM:', '1PM:', '2PM:', '3PM:', '4PM:', '5PM:', '6PM:', '7PM:', 'Total:'];
+function hours(){
+  var time = ['6AM:', '7AM:', '8AM:', '9AM:', '10AM:', '11AM:', '12PM:', '1PM:', '2PM:', '3PM:', '4PM:', '5PM:', '6PM:', '7PM:', 'Total:']
   var table = document.getElementsByClassName('operation_hours');
   var operationArr = [];
 
@@ -38,7 +38,8 @@ function hours()
       '<td>' + time[11] + '</td>' +
       '<td>' + time[12] + '</td>' +
       '<td>' + time[13] + '</td>' +
-      '<td>' + time[14] + '</td>'
+      '<td>' + time[14] + '</td>' +
+      '<td>' + time[15] + '</td>'
     );
   }
 
@@ -53,8 +54,10 @@ function hours()
 console.log(hours);
 
  //add business location and details
- var pikePlace = new Store('First and Pike', 23, 65, 6.3);
- var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
- var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
- var capitolHill = new Store('Capitol Hill', 20, 28, 2.3);
- var alki = new Store('Alki', 2, 16, 4.6);
+var pikePlace = new Store('First and Pike', 23, 65, 6.3);
+var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capitolHill = new Store('Capitol Hill', 20, 28, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
+
+var
