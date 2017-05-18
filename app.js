77ff9d11bbaefafa31dc8cbdc2bd64e2b5416cbf
Loaye <jr.carstens00@gmail.com>
2017-05-18 16:29:17 -0700

    added two files and table content

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ Store.prototype.hourly = function(){
   return Math.floor(people * this.avg);
 };
 
+//Function that takes total sales and pushes it back to Constructor
 Store.prototype.generateSales = function(){
   for (var i = 0; i < 15; i++){
     this.cookieSales.push(this.hourly());
   }
 };
+
 //add business building and details
 var pikePlace = new Store('First and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
@@ -29,39 +31,40 @@ var capitolHill = new Store('Capitol Hill', 20, 28, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
 var building = [pikePlace, seaTac, seattleCenter, capitolHill, alki];
-
 for (var i = 0; i < building.length; i++){
   building[i].generateSales();
 }
 
-// var table = document.getElementsByClassName('shell');
-// var data = [];
+Store.prototype.getSales = function(){
+  var table = document.getElementsByClassName('shell');
+  var data = [];
 
-// for (var i = 0; i < building.length; i++){
-//   data.push(
-//     '<td>' + building + '</td>' +
-//     '<td>' +  + '</td>' +
-//     '<td>' + time[2] + '</td>' +
-//     '<td>' + time[3] + '</td>' +
-//     '<td>' + time[4] + '</td>' +
-//     '<td>' + time[5] + '</td>' +
-//     '<td>' + time[6] + '</td>' +
-//     '<td>' + time[7] + '</td>' +
-//     '<td>' + time[8] + '</td>' +
-//     '<td>' + time[9] + '</td>' +
-//     '<td>' + time[10] + '</td>' +
-//     '<td>' + time[11] + '</td>' +
-//     '<td>' + time[12] + '</td>' +
-//     '<td>' + time[13] + '</td>' +
-//     '<td>' + time[14] + '</td>' +
-//     '<td>' + time[15] + '</td>'
-//   );
-// }
+  for (var i = 0; i < building.length; i++){
+    data.push(
+      '<td>' + this.cookieSales[0] + '</td>' +
+      '<td>' + this.cookieSales[1] + '</td>' +
+      '<td>' + this.cookieSales[2] + '</td>' +
+      '<td>' + this.cookieSales[3] + '</td>' +
+      '<td>' + this.cookieSales[4] + '</td>' +
+      '<td>' + this.cookieSales[5] + '</td>' +
+      '<td>' + this.cookieSales[6] + '</td>' +
+      '<td>' + this.cookieSales[7] + '</td>' +
+      '<td>' + this.cookieSales[8] + '</td>' +
+      '<td>' + this.cookieSales[9] + '</td>' +
+      '<td>' + this.cookieSales[10] + '</td>' +
+      '<td>' + this.cookieSales[11] + '</td>' +
+      '<td>' + this.cookieSales[12] + '</td>' +
+      '<td>' + this.cookieSales[13] + '</td>' +
+      '<td>' + this.cookieSales[14] + '</td>' +
+      '<td>' + this.cookieSales[15] + '</td>'
+    );
+  }
 
-// var new_row;
-//
-// for (var j = 0; j < data.length; j++){
-//   new_row = document.createElement('tr');
-//   new_row.innerHTML = data[j];
-//   table.appendChild(new_row);
-// }
+  var new_row;
+
+  for (var j = 0; j < data.length; j++){
+    new_row = document.createElement('tr');
+    new_row.innerHTML = data[j];
+    table.appendChild(new_row);
+  }
+};
