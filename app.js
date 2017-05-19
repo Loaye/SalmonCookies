6c8f84198a64eaf7aa257d969bc5ebbb603fada8
Loaye <jr.carstens00@gmail.com>
2017-05-18 17:44:45 -0700

    displaying tables

--- app.js
+++ app.js
@@ -32,41 +32,29 @@ var alki = new Store('Alki', 2, 16, 4.6);
 
 var building = [pikePlace, seaTac, seattleCenter, capitolHill, alki];
 
-for (var i = 0; i < building.length; i++){
-  building[i].generateSales();
-  console.log(building[i].cookieSales);
-}
+
 
 Store.prototype.getSales = function(){
-  var table = document.getElementsByClassName('shell');
+  var table = document.getElementById('shell');
   var data = [];
+
+  data.push('<td>' + this.building + '</td>');
 
   for (var i = 0; i <this.cookieSales.length; i++){
     data.push(
-      '<td>' + this.cookieSales[0] + '</td>' +
-      '<td>' + this.cookieSales[1] + '</td>' +
-      '<td>' + this.cookieSales[2] + '</td>' +
-      '<td>' + this.cookieSales[3] + '</td>' +
-      '<td>' + this.cookieSales[4] + '</td>' +
-      '<td>' + this.cookieSales[5] + '</td>' +
-      '<td>' + this.cookieSales[6] + '</td>' +
-      '<td>' + this.cookieSales[7] + '</td>' +
-      '<td>' + this.cookieSales[8] + '</td>' +
-      '<td>' + this.cookieSales[9] + '</td>' +
-      '<td>' + this.cookieSales[10] + '</td>' +
-      '<td>' + this.cookieSales[11] + '</td>' +
-      '<td>' + this.cookieSales[12] + '</td>' +
-      '<td>' + this.cookieSales[13] + '</td>' +
-      '<td>' + this.cookieSales[14] + '</td>' +
-      '<td>' + this.cookieSales[15] + '</td>'
+      '<td>' + this.cookieSales[i] + '</td>'
       );
   }
 
   var new_row;
-
-  for (var j = 0; j < data.length; j++){
-    new_row = document.createElement('tr');
-    new_row.innerHTML = data[j];
-    table.appendChild(new_row);
-  }
+  new_row = document.createElement('tr');
+  new_row.innerHTML = data.join('');
+  console.log(new_row);
+  console.log(table);
+  table.appendChild(new_row);
 };
+
+for (var i = 0; i < building.length; i++){
+  building[i].generateSales();
+  building[i].getSales();
+}
